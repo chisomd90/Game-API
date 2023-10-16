03c412be4db670ed42d94f66356116d1174c2347
chisomd90 <chisomd90@gmail.com>
2023-10-16 23:11:46 +0100

    GAME_API

--- game_backend/settings.py
+++ game_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jb1*qjgfq^dih^uh#@2wy(r4rc#ou!5+xklo8peij(^#7@90b8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stone-4asd.onrender.com']
 
 
 # Application definition
